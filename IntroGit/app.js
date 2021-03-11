a9ae674a34eede254e437f86215193ccc51937a4
CarZamora99 <czamoraarreola@gmail.com>
2021-03-11 10:36:18 -0600

    Solucion de problemas

--- IntroGit/app.js
+++
@@ -1,2 +0,0 @@
-let titulo=document.getElementById('Titulo');
-titulo.innerHtml = "Carlos Zamora esta aprendiendo Git con comandos";
---
+++ app.js
@@ -0,0 +1,3 @@
+let titulo=document.getElementById('titulo');
+
+titulo.innerHTML="Carlos Zamora está aprendiendo Git por linea de comandos";
